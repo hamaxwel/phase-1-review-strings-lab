a37b58a615f6c08d4bd91e24906713ad1988dfca
maxwel kirimi <maxwelkirimii@gmail.com>
2024-09-24 12:47:42 +0300

    document structure

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser = 'future grasshopper' ;
+
+const welcomeMessage = 'Welcome to Flatbook, future grasshopper!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, FUTURE GRASSHOPPER!'
+
+const shortGreeting = 'Welcome, future!'
+
